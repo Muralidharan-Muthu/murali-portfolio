97d9e5ff32ed0647aded17194040bcbaa49bb9b3
Muralidharan <muralimuthu047@gmail.com>
2025-07-01 13:05:44 +0530

    Portfolio

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -114,12 +114,32 @@ const Contact = () => {
             />
           </label>
 
-          <button
-            type='submit'
-            className='bg-tertiary py-3 px-8 rounded-xl outline-none w-fit text-white font-bold shadow-md shadow-primary'
-          >
-            {loading ? "Sending..." : "Send"}
-          </button>
+
+
+          <div className="flex flex-row justify-between items-center mt-6">
+            <button
+              type='submit'
+              className='bg-tertiary py-3 px-8 rounded-xl outline-none w-fit text-white font-bold shadow-md shadow-primary'
+            >
+              {loading ? "Sending..." : "Send"}
+            </button>
+            <div className="flex flex-row gap-6">
+              <a
+                href="https://www.linkedin.com/in/muralidharan-muthu/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-blue-400 hover:scale-110 transition-transform bg-tertiary py-3 px-4 rounded-xl shadow-md shadow-primary"
+              >
+                <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="currentColor" viewBox="0 0 24 24"><path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-10h3v10zm-1.5-11.268c-.966 0-1.75-.784-1.75-1.75s.784-1.75 1.75-1.75 1.75.784 1.75 1.75-.784 1.75-1.75 1.75zm13.5 11.268h-3v-5.604c0-1.337-.025-3.063-1.868-3.063-1.868 0-2.154 1.459-2.154 2.968v5.699h-3v-10h2.881v1.367h.041c.401-.761 1.379-1.563 2.838-1.563 3.036 0 3.6 2 3.6 4.591v5.605z" /></svg>
+              </a>
+              <a
+                href="mailto:muralidharanmuthu2@gmail.com"
+                className="text-red-400 hover:scale-110 transition-transform bg-tertiary py-3 px-4 rounded-xl shadow-md shadow-primary"
+              >
+                <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="currentColor" viewBox="0 0 24 24"><path d="M12 13.065l-11.985-8.065h23.97l-11.985 8.065zm-12-7.065v14h24v-14l-12 8.084-12-8.084z" /></svg>
+              </a>
+            </div>
+          </div>
         </form>
       </motion.div>
 
