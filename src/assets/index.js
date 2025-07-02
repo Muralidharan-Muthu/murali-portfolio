10571db1a89ce31b9a79a32d856aaf2c8ed3636a
Muralidharan <muralimuthu047@gmail.com>
2025-07-02 10:39:12 +0530

    Portfolio

--- src/assets/index.js
+++ src/assets/index.js
@@ -6,6 +6,7 @@ import web from "./web.png";
 import github from "./github.png";
 import menu from "./menu.svg";
 import close from "./close.svg";
+import leetcode from "./leetcode.svg";
 
 import css from "./tech/css.png";
 import docker from "./tech/docker.png";
@@ -24,6 +25,7 @@ import python from "./tech/python.png"
 import imagegenerator from "./imagegenerator.png"
 
 export {
+  leetcode,
   logo,
   backend,
   creator,
